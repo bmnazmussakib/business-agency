fdd96022fb8ee8b39437631ab0a3108a93969249
Nazmus Sakib <nazmussakib161@gmail.com>
2022-04-02 21:15:16 +0600

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,29 @@ $(document).ready(function () {
   });
 
 
+  // Advisor carousel
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    // margin: 10,
+    items: 1,
+    nav: true,
+    navText: ["<i class='fa-solid fa-arrow-left-long'></i>", "<i class='fa-solid fa-arrow-right-long'></i>"],
+
+    // responsive: {
+    //   0: {
+    //     items: 1
+    //   },
+    //   600: {
+    //     items: 3
+    //   },
+    //   1000: {
+    //     items: 5
+    //   }
+    // }
+
+  });
+
+
 
 
 });
